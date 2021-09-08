3a6a9645fc78faab681b83b6ad58cac4756250eb
QuentinBisson <quentin@giantswarm.io>
2021-09-08 17:12:54 +0200

    Add argo app

---
+++ Makefile.custom.mk
@@ -0,0 +1,46 @@
+AUTOGENMSG := \# This is an auto-generated file. DO NOT EDIT
+
+MANIFESTS := $(shell find manifests)
+
+KUSTOMIZE := bin/kustomize
+
+.PHONY: all
+all: manifests/bases/upstream-argocd bootstrap
+
+.PHONY: manifests/bases/upstream-argocd
+manifests/bases/upstream-argocd: ARGOCD_VERSION := v2.1.0
+manifests/bases/upstream-argocd: ARGOCD_REPOSITORY := https://github.com/argoproj/argo-cd.git
+manifests/bases/upstream-argocd: WORK_DIR := $(shell mktemp -d -t ci-XXXXXXXXXX)
+manifests/bases/upstream-argocd: $(KUSTOMIZE)
+	@echo "====> $@"
+	git -c advice.detachedHead=false clone --quiet --depth 1 --branch ${ARGOCD_VERSION} ${ARGOCD_REPOSITORY} ${WORK_DIR}
+	mkdir -p manifests/bases/upstream-argocd
+	echo "$(AUTOGENMSG)" > manifests/bases/upstream-argocd/install.yaml
+	$(KUSTOMIZE) build "$(WORK_DIR)/manifests/cluster-install" >> manifests/bases/upstream-argocd/install.yaml
+	rm -rf $(WORK_DIR)
+
+BOOTSRAP_DEPS :=
+BOOTSRAP_DEPS += bootstrap/j2vo9.yaml
+bootstrap: $(BOOTSRAP_DEPS)
+
+bootstrap/%.yaml: $(KUSTOMIZE) $(MANIFESTS)
+	@echo "====> $@"
+	mkdir -p bootstrap
+	echo "$(AUTOGENMSG)" > $@
+	$(KUSTOMIZE) build manifests/cluster/$* >> $@
+
+$(KUSTOMIZE): ## Download kustomize locally if necessary.
+	@echo "====> $@"
+	$(call go-get-tool,$(KUSTOMIZE),sigs.k8s.io/kustomize/kustomize/v4@v4.1.2)
+
+# go-get-tool will 'go get' any package $2 and install it to $1.
+PROJECT_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
+define go-get-tool
+set -e ;\
+TMP_DIR=$$(mktemp -d) ;\
+cd $$TMP_DIR ;\
+go mod init tmp ;\
+echo "Downloading $(2)" ;\
+GOBIN=$(PROJECT_DIR)/bin go get $(2) ;\
+rm -rf $$TMP_DIR
+endef
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # argocd-poc
-POC to install argo on Workload clusters
+
+POC to install argocd on Workload clusters.
+
+
+## Layout
+
+- AppProject: `argocd` contains:
+  - Application: `argocd` manages itself:
+    -  upstream Argo CD manifests
+    - `argocd` AppProject CR
+    - `argocd` Application CR
+
+## Updating Argo CD manifests
+
+Run
+
+```
+make all
+```
+
+This make target will:
+  - update `manifests/bases/upstream-argocd/` with version of ArgoCD specified in Makefile.
+  - render `bootstrap/argocd.yaml` file with the project for self-managed provider application.
+
+Those directories' content can be rendered separately using different make targets:
+  - `make manifests/bases/upstream-argocd` - updates upstream ArgoCD.
+  - `make bootstrap` - updates `bootstrap/` resources.
+
+To change the version of rendered manifests, update `ARGOCD_VERSION` in [Makefile.custom.mk](Makefile.custom.mk).
+You might have to change image override in `manifests/argocd/kustomization.yaml` as well.
